718b32395d7592f48fc8af8f5f42261c579e7b86
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-05-30 09:53:38 -0400

    linq

--- S25W4IntroToLinq/Program.cs
+++ S25W4IntroToLinq/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = { 4, 3, 5, 7, 8, 6, 3, 2, 4, 6, 8, 9, 1 };
+
+            // query syntax
+            var greaterThan4 = from num in array
+                               where num > 4
+                               orderby num descending
+                               select num;
+
+            // method syntax
+            var greaterThan5 = array.Where(n => n > 5).OrderBy(n => n).ToList();
+
+            foreach (var i in greaterThan5)
+                Console.Write(i + " ");
+            Console.WriteLine("\n\n\n");
+
+
+
+            List<string> colors = new List<string>();
+            colors.Add("rUSt");
+            colors.Add("bLuE");
+            colors.Add("GreEn");
+            colors.Add("ReD");
+
+
         }
     }
 }
